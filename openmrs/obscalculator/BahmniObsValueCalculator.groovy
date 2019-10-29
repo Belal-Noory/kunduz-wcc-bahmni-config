c90d7501b16331313a6c2459ac6561d04b5308d7
R-VdP <ramses.de.norre@brussels.msf.org>
2019-10-29 12:29:08 +0100

    Include the obs calculator file with an empty implementation.

---
+++ openmrs/obscalculator/BahmniObsValueCalculator.groovy
@@ -0,0 +1,9 @@
+import org.openmrs.module.bahmniemrapi.obscalculator.ObsValueCalculator;
+import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniEncounterTransaction
+
+public class BahmniObsValueCalculator implements ObsValueCalculator {
+
+    public void run(BahmniEncounterTransaction bahmniEncounterTransaction) {}
+
+}
+
