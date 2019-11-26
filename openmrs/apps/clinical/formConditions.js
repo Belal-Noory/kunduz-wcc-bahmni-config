199201d82d006973a2d9b83926f5915f5b17c04e
R-VdP <r-vdp@users.noreply.github.com>
2019-11-26 09:52:32 +0430

    FormConditions generic template.

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,4 +1,31 @@
-Bahmni.ConceptSet.FormConditions.rules = {
+
+const makeCondition = (question, determiningAnswer, otherQuestion) => {
+  Bahmni.ConceptSet.FormConditions.rules[question] = (formName, formFieldValues) => {
+    var conditions = {
+      show: [],
+      hide: []
+    };
+
+    const value = formFieldValues[question];
+    if (value == determiningAnswer) {
+      conditions.show.push(otherQuestion);
+    } else {
+      conditions.hide.push(otherQuestion);
+    }
+    return conditions;
+  }
 };
+
+Bahmni.ConceptSet.FormConditions.rules = {};
+
+[
+  {
+    question:          'forms.discharge.reasonLostFU',
+    determiningAnswer: 'forms.discharge.reasonLostFU.other',
+    otherQuestion:     'forms.discharge.reasonLostFU.otherDetail'
+  }
+].forEach(field => {
+  makeCondition(field.question, field.determiningAnswer, field.otherQuestion)
+})
 
 
