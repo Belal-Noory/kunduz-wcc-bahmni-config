233cc37baccf85b18a24525515fc36621ba13c77
Belal-Noory <belalnoory2@gmail.com>
2019-11-26 21:41:55 +0430

    first try

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,31 +1,37 @@
-
 const makeCondition = (question, determiningAnswer, otherQuestion) => {
-  Bahmni.ConceptSet.FormConditions.rules[question] = (formName, formFieldValues) => {
-    var conditions = {
-      show: [],
-      hide: []
-    };
+    Bahmni.ConceptSet.FormConditions.rules[question] = (formName, formFieldValues) => {
+        var conditions = {
+            show: [],
+            hide: []
+        };
 
-    const value = formFieldValues[question];
-    if (value == determiningAnswer) {
-      conditions.show.push(otherQuestion);
-    } else {
-      conditions.hide.push(otherQuestion);
+        const value = formFieldValues[question];
+        if (value == determiningAnswer) {
+            conditions.show.push(otherQuestion);
+        } else {
+            conditions.hide.push(otherQuestion);
+        }
+        return conditions;
     }
-    return conditions;
-  }
 };
 
 Bahmni.ConceptSet.FormConditions.rules = {};
 
-[
-  {
-    question:          'forms.discharge.reasonLostFU',
-    determiningAnswer: 'forms.discharge.reasonLostFU.other',
-    otherQuestion:     'forms.discharge.reasonLostFU.otherDetail'
-  }
+[{
+        question: 'forms.discharge.reasonLostFU',
+        determiningAnswer: 'forms.discharge.reasonLostFU.other',
+        otherQuestion: 'forms.discharge.reasonLostFU.otherDetail'
+    },
+    {
+        question: 'clinical.pain.treatment',
+        determiningAnswer: 'clinical.pain.treatment.other',
+        otherQuestion: 'clinical.pain.treatment.other_details'
+    },
+    {
+        question: 'clinical.woundSpecific.type',
+        determiningAnswer: 'clinical.woundSpecific.type.other',
+        otherQuestion: 'clinical.woundSpecific.type.otherAnswer'
+    }
 ].forEach(field => {
-  makeCondition(field.question, field.determiningAnswer, field.otherQuestion)
+    makeCondition(field.question, field.determiningAnswer, field.otherQuestion)
 })
-
-
