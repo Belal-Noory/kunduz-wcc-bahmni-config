a320158323cc6ae96b6c7f50267b0be90cfe69e5
R-VdP <r-vdp@users.noreply.github.com>
2019-11-26 15:17:48 +0430

    Validation of the phone number.

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -1,15 +1,15 @@
 Bahmni.Registration.customValidator = {
-    "age.days": {
-        method: function (name, value) {
-            return value >= 0;
-        },
-        errorMessage: "REGISTRATION_AGE_ERROR_KEY"
+  "age.days": {
+    method: function (name, value) {
+      return value >= 0;
     },
-    "Telephone Number": {
-        method: function (name, value, personAttributeDetails) {
-            return value && value.length> 6;
-        },
-        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
-    }
+    errorMessage: "REGISTRATION_AGE_ERROR_KEY"
+  },
+  "reg.phoneNumber": {
+    method: function (name, value, personAttributeDetails) {
+      return value && (value.length = 0 || value.length = 10);
+    },
+    errorMessage: "The telephone number should be either or empty, or contain 10 digits."
+  }
 };
 
