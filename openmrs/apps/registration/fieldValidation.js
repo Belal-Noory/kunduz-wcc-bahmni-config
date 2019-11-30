ae683224f89ad2d275170fa4f6e340d0886e7855
RdN <141248+R-VdP@users.noreply.github.com>
2019-11-30 11:41:53 +0000

    Fix syntax error.

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -7,7 +7,7 @@ Bahmni.Registration.customValidator = {
   },
   "reg.phoneNumber": {
     method: function (name, value, personAttributeDetails) {
-      return value && (value.length = 0 || value.length = 10);
+      return value && (value.length == 0 || value.length == 10);
     },
     errorMessage: "The telephone number should be either or empty, or contain 10 digits."
   }
