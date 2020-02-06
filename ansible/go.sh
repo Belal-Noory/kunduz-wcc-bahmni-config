aa0ab05ca109ab428a3dfdedd77cc77b27a4224d
R-VdP <ramses.de.norre@brussels.msf.org>
2020-02-06 15:44:48 +0100

    Fix the wound dressing query.

--- ansible/go.sh
+++ ansible/go.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventory config.yml
+if [ -f /etc/NIXOS ]; then
+  nix run nixos.ansible --command ansible-playbook -i inventory config.yml $@
+else
+  ansible-playbook -i inventory config.yml $@
+fi
 
